d624c352d4e53ca51eadf32cd9e0198780c92dd1
MaheshK04 <maheshk04x@gmail.com>
2024-03-06 17:35:17 +0530

    Modified getstarted component and added css

--- src/components/GetStarted/GetStarted.jsx
+++ src/components/GetStarted/GetStarted.jsx
@@ -1,9 +1,24 @@
 import React from "react";
-
+import dp from "../../assets/image.png";
+import "./GetStarted.css";
 function GetStarted() {
   return (
     <div>
-      <h1>Get Started with KoinX for FREE</h1>
+      <p className="head">
+        Get Started with KoinX <br></br> for FREE
+      </p>
+      <p className="para">
+        with our range of features that you can equip for
+        <br />
+        free, KoinX allows you to be more educated and aware of your tax
+        reports.
+      </p>
+      <img src={dp} alt="" style={{ width: "300px", height: "250px" }} />
+      <div style={{ textAlign: "center" }}>
+        <button className="btn">
+          <p>Get Started for FREE</p>
+        </button>
+      </div>
     </div>
   );
 }
