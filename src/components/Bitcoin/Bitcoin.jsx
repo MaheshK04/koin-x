becd687e0b8778991147e423999d687cd8574c97
MaheshK04 <maheshk04x@gmail.com>
2024-03-06 10:51:45 +0530

    Make API call to fetch bticoin price data

--- src/components/Bitcoin/Bitcoin.jsx
+++ src/components/Bitcoin/Bitcoin.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import bc from "../../assets/Bitcoin.webp";
+import { price } from "../config/api";
+import axios from "axios";
+
 
 function Bitcoin() {
+  const [priceData, setPriceData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(price)
+      .then((response) => {
+        //console.log(response);
+        setPriceData(response.data);
+      })
+      .catch((error) => console.log("Error"));
+  }, []); 
+
+  if (priceData === null) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       {/* <img src={bc} style={{ width: "30px" }}></img>
@@ -23,6 +42,8 @@ function Bitcoin() {
           Rank #1
         </button>
       </div>
+      <h1>${priceData.bitcoin.usd}</h1>
+      <h3>₹{priceData.bitcoin.inr}</h3>
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis
         perspiciatis voluptatum dignissimos aspernatur, delectus optio illo
---
+++ src/components/config/api.js
@@ -0,0 +1,3 @@
+
+export const price =
+  "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=inr%2Cusd&include_24hr_change=true&precision=2";
