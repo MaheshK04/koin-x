7f66e6efc4648b5d559859dd2141234f92b57d00
MaheshK04 <maheshk04x@gmail.com>
2024-03-07 12:38:34 +0530

    Added Team members in Team component

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -6,12 +6,59 @@ function Team() {
   return (
     <div>
       <h2>Team</h2>
-      <div>
-        <img src={img} alt="cant find"></img>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus
-        distinctio blanditiis assumenda qui nulla iusto, repellat, harum et
-        minus molestiae id necessitatibus doloribus reiciendis dolorem? Ab,
-        adipisci explicabo! Est, laboriosam?
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+        recusandae facere nam sit harum repellat, minima accusantium architecto.
+        Quas qui doloribus consequatur architecto distinctio. Quidem quasi
+        aliquid architecto quae quo.
+      </p>
+      <div className="container">
+        <div className="image-container">
+          <img src={img} alt="Your Image" />
+          <a>John K</a>
+          <br />
+          <a>SDE-1</a>
+        </div>
+        <div className="text-container">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            itaque nemo magni sint, dolore ab in, possimus adipisci iusto
+            dignissimos vitae expedita repudiandae hic saepe distinctio at esse
+            consectetur eveniet?
+          </p>
+        </div>
+      </div>
+      <div className="container">
+        <div className="image-container">
+          <img src={img} alt="Your Image" />
+          <a>John K</a>
+          <br />
+          <a>SDE-1</a>
+        </div>
+        <div className="text-container">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            itaque nemo magni sint, dolore ab in, possimus adipisci iusto
+            dignissimos vitae expedita repudiandae hic saepe distinctio at esse
+            consectetur eveniet?
+          </p>
+        </div>
+      </div>
+      <div className="container">
+        <div className="image-container">
+          <img src={img} alt="Your Image" />
+          <a>John K</a>
+          <br />
+          <a>SDE-1</a>
+        </div>
+        <div className="text-container">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            itaque nemo magni sint, dolore ab in, possimus adipisci iusto
+            dignissimos vitae expedita repudiandae hic saepe distinctio at esse
+            consectetur eveniet?
+          </p>
+        </div>
       </div>
     </div>
   );
